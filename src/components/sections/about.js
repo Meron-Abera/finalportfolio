c73f34d8b1c2763484248531bad59d3178b84b29
Nathnaelc <nathnaelc@uni.minerva.edu>
2024-04-30 19:15:19 -0400

    sharp module avif fix

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -180,10 +180,10 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/nathnael.jpg"
+              src="../../images/nathe.png"
               width={500}
               quality={95}
-              formats={['AUTO', 'WEBP', 'AVIF']}
+              formats={['AUTO', 'WEBP']}
               alt="Headshot"
             />
           </div>
