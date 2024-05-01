698f5a79e9ec03aa24f1a19a9e908c4ea7db0a14
Nathnaelc <nathnaelc@uni.minerva.edu>
2024-05-01 17:44:31 -0400

    gatsby config fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,8 @@ module.exports = {
               maxWidth: 700,
               linkImagesToOriginal: true,
               quality: 90,
-              tracedSVG: { color: config.colors.green },
+              withWebp: true,
+              withAvif: true,
             },
           },
           {
